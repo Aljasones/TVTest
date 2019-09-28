7dad3c5f0136d95600222eb19b4d2775163dc370
Student <student@itpark>
2019-09-28 18:59:09 +0300

    first

---
+++ src/main/java/ru/itpark/Main.java
@@ -0,0 +1,14 @@
+package ru.itpark;
+
+public class Main {
+    public static void main(String[] args) {
+        TV tv = new TV();
+        System.out.println(tv.getCurrentVolume());
+        tv.setCurrentChannel(50);
+        System.out.println(tv.getCurrentChannel());
+        tv.setCurrentChannel(60);
+        System.out.println(tv.getCurrentChannel());
+        tv.setCurrentChannel(2000);
+        System.out.println(tv.getCurrentChannel());
+
+}}
---
+++ src/test/java/ru/itpark/TVTest.java
@@ -0,0 +1,17 @@
+package ru.itpark;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TVTest {
+    @Test
+    public void shouldSetCurrentChannelWhenVolumeOutOfBounds(){
+        TV tv = new TV();
+        int before = tv.getCurrentChannel();
+        tv.setCurrentChannel(1000);
+
+        int actual = tv.getCurrentChannel();
+        assertEquals(before, actual);
+    }
+}
